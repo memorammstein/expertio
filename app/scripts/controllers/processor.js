eaa12acd7ced3b71c1ff058742bab10fb6527121
Guillermo Iván Jiménez Ruvalcaba <engel_live_aus_rammsteinland@hotmail.com>
2015-03-13 10:22:53 -0600

    beta1

--- app/scripts/controllers/processor.js
+++ app/scripts/controllers/processor.js
@@ -17,10 +17,48 @@ angular.module('expertioApp')
 
   	reader.onload = function () {
   		$scope.data.raw = reader.result;
-  	}
+  		$scope.data.read = $scope.data.raw
+  							.replace(/\s*$^\s*/gm, '\n')
+  							.replace(/[ \t]+/g, ' ')
+  							.replace(/\s*:\s*/g, ':')
+  							.replace(/\s*=\s*/g, '=')
+  							.replace(/\s*<\s*/g, '<')
+  							.replace(/\s*>\s*/g, '>')
+  							.replace(/\s*IF\s*/g, 'IF')
+  							.replace(/\s*AND\s*/g, 'AND')
+  							.replace(/\s*THEN\s*/g, 'THEN');
+  		$scope.data.read = $scope.data.read.split('\n');
+  	};
 
   	$scope.read = function () {
   		reader.readAsText($scope.fileModel);
+  	};
+
+  	$scope.process = function (arreglo) {
+  		$scope.data.processed = [/*{ruleFormat:'{name:"yolo",if:[1,2,3],then:"x=y"}'}*/];
+  		arreglo.forEach(function (element) {
+  			var splited = element.split(':');
+  			var rule = {};
+  			rule.name = splited[0];
+  			splited = splited[1].split('THEN');
+  			var tempIf = splited[0].replace('IF','').split(/AND/g);
+  			rule.if = [];
+  			tempIf.forEach(function (str) {
+  				rule.if.push(processCompairInString(str));
+  			});
+  			rule.then = processResultInString(splited[1]);
+  			$scope.data.processed.push(rule);
+  		});
+  	};
+
+  	function processCompairInString (string) {
+  		var splitedEq = string.split(/(=|<|>)/);
+  		return {compare:splitedEq[0], using:splitedEq[1], against:splitedEq[2]};
+  	}
+
+  	function processResultInString (string) {
+  		var splitedEq = string.split(/=/);
+  		return {variable:splitedEq[0], equals:splitedEq[1]};
   	}
 
   });
